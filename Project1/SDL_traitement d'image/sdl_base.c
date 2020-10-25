a1b3935947595ef51264a765096e357d437bbbfe
paul.copigneaux <paul.copigneaux@epita.fr>
2020-10-25 13:06:39 +0100

    plus trop de copier coller

--- Project1/SDL_traitement d'image/sdl_base.c
+++ Project1/SDL_traitement d'image/sdl_base.c
@@ -3,62 +3,66 @@
 #include "SDL/SDL_image.h"
 
 static inline
-Uint8* pixel_ref(SDL_Surface *surf, unsigned x, unsigned y)
+Uint8* pixel_ref(SDL_Surface *surface, unsigned x, unsigned y)
 {
-    int bpp = surf->format->BytesPerPixel;
-    return (Uint8*)surf->pixels + y * surf->pitch + x * bpp;
+    int bpp =surface->format->BytesPerPixel;
+    return (Uint8*)surface->pixels + y *surface->pitch + x * bpp;
 }
 
 void init_sdl()
 {
-    if(SDL_Init(SDL_INIT_VIDEO) == -1)
-        errx(1,"Could not initialize SDL: %s.\n", SDL_GetError());
+    if (SDL_Init(SDL_INIT_VIDEO)== -1) {
+        errx(1,"Unable to initialize SDL: %s.\n", SDL_GetError());
+      }
 }
 
 void SDL_FreeSurface(SDL_Surface *surface);
 
 SDL_Surface* load_image(char *path)
 {
-    SDL_Surface *img;
+    SDL_Surface *image;
+    image= IMG_Load(path);
+    if (!image) {
+        errx(3, "ereur de load %s: %s", path, IMG_GetError());
+    }
 
-    img = IMG_Load(path);
-    if (!img)
-        errx(3, "can't load %s: %s", path, IMG_GetError());
-
-    return img;
+    return image;
 }
 
 Uint32 get_pixel(SDL_Surface *surface, unsigned x, unsigned y)
 {
-    Uint8 *p = pixel_ref(surface, x, y);
-
-    switch (surface->format->BytesPerPixel)
-    {
+    Uint8 *p =pixel_ref(surface, x, y);
+    switch ( surface->format->BytesPerPixel) {
         case 1:
             return *p;
+            break;
 
         case 2:
             return *(Uint16 *)p;
+            break;
 
         case 3:
-            if (SDL_BYTEORDER == SDL_BIG_ENDIAN)
+            if (SDL_BIG_ENDIAN == SDL_BYTEORDER ) {
                 return p[0] << 16 | p[1] << 8 | p[2];
-            else
+            }
+            else {
                 return p[0] | p[1] << 8 | p[2] << 16;
+            }
+            break;
 
         case 4:
             return *(Uint32 *)p;
-    }
+            break;
 
-    return 0;
+        default:
+            return 0;
+    }
 }
 
 void put_pixel(SDL_Surface *surface, unsigned x, unsigned y, Uint32 pixel)
 {
     Uint8 *p = pixel_ref(surface, x, y);
-
-    switch(surface->format->BytesPerPixel)
-    {
+    switch(surface->format->BytesPerPixel) {
         case 1:
             *p = pixel;
             break;
@@ -68,14 +72,12 @@ void put_pixel(SDL_Surface *surface, unsigned x, unsigned y, Uint32 pixel)
             break;
 
         case 3:
-            if(SDL_BYTEORDER == SDL_BIG_ENDIAN)
-            {
+            if(SDL_BIG_ENDIAN == SDL_BYTEORDER ) {
                 p[0] = (pixel >> 16) & 0xff;
-                p[1] = (pixel >> 8) & 0xff;
+                p[1]= (pixel >> 8) & 0xff;
                 p[2] = pixel & 0xff;
             }
-            else
-            {
+            else {
                 p[0] = pixel & 0xff;
                 p[1] = (pixel >> 8) & 0xff;
                 p[2] = (pixel >> 16) & 0xff;
