c7679c7a30bda776cf0e2a0cada2b3722729ca65
Jeanne Cai <jeanne.cai@epita.fr>
2020-12-15 19:21:47 +0100

    change name repo

--- Project1/GTK/segmentation/canny.h
+++
@@ -1,6 +0,0 @@
-#ifndef CANNY_H
-#define CANNY_H
-
-void canny_edge_detection(SDL_Surface *image_surface);
-
-#endif
--- Project1/NeuralNetwork/include/tools.h
+++
@@ -1,7 +0,0 @@
-#ifndef TOOLS_H
-#define TOOLS_H
-
-void cleanBuffer();
-void secuScanf(char* format, void* ref);
-
-#endif
