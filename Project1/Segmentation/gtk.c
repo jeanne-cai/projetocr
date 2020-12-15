8ee89d9b6d9589e0989ccf1e1080e05e78c38b95
Jeanne Cai <jeanne.cai@epita.fr>
2020-12-15 01:08:27 +0100

    Save file up

--- Project1/Segmentation/gtk.c
+++ Project1/Segmentation/gtk.c
@@ -22,7 +22,7 @@ gchar *filename = "";
 GtkWidget* create_file_chooser_dialog(GtkFileChooserAction action)
 {
     GtkWidget *dialog = NULL;
-    switch(action)
+    switch (action)
     {
         case GTK_FILE_CHOOSER_ACTION_OPEN:
             dialog = gtk_file_chooser_dialog_new("Open File",
@@ -31,7 +31,7 @@ GtkWidget* create_file_chooser_dialog(GtkFileChooserAction action)
                     "Open", GTK_RESPONSE_ACCEPT,
                     NULL); break;
         case GTK_FILE_CHOOSER_ACTION_SAVE:
-            gtk_file_chooser_dialog_new ("Save File",
+            dialog = gtk_file_chooser_dialog_new("Save File",
                     NULL, action,
                     "Cancel", GTK_RESPONSE_CANCEL,
                     "Save", GTK_RESPONSE_ACCEPT,
@@ -76,7 +76,6 @@ void load_file(gchar *file)
             gtk_image_set_from_file(GTK_IMAGE(image), filename);
 
         gtk_window_resize(GTK_WINDOW(window), w, h);
-//        gtk_window_set_position(GTK_WINDOW(window), GTK_WIN_POS_CENTER);
     }
 
     SDL_FreeSurface(image_surface);
@@ -99,9 +98,10 @@ void choose_file()
     gtk_widget_destroy(dialog);
 }
 
+
 // Save file
 
-/*void save_file()
+void save_file(char *text)
 {
     GtkWidget *dialog;
     GtkFileChooser *chooser;
@@ -109,16 +109,55 @@ void choose_file()
     dialog = create_file_chooser_dialog(GTK_FILE_CHOOSER_ACTION_SAVE);
     chooser = GTK_FILE_CHOOSER(dialog);
 
-    if (gtk_dialog_run(GTK_DIALOG(dialog))  == GTK_RESPONSE_ACCEPT)
+    if (gtk_dialog_run(GTK_DIALOG(dialog)) == GTK_RESPONSE_ACCEPT)
     {
-        char *filename;
-        filename = gtk_file_chooser_get_filename(chooser);
-        //save_to_file (filename);
-        g_free(filename);
+        char *text_filename;
+        text_filename = gtk_file_chooser_get_filename(chooser);
+
+        FILE *fichier = NULL;
+        fichier = fopen(text_filename, "r");
+
+        if (fichier == NULL)
+        {
+            text_filename = "OCR_text.txt";
+            GtkWidget *alerte = gtk_message_dialog_new(NULL,
+                        GTK_DIALOG_DESTROY_WITH_PARENT,
+                        GTK_MESSAGE_INFO, GTK_BUTTONS_OK,
+            "It's not a text file name.\nSaved under the name file : \"OCR_text.txt\".");
+            gtk_dialog_run(GTK_DIALOG(alerte));
+            gtk_widget_destroy(alerte);
+        }
+        else
+        {
+            text_filename = strrchr(text_filename, '.');
+            strcpy(text_filename, ".txt");
+            fclose(fichier);
+        }
+        g_file_set_contents(text_filename, text, strlen(text), NULL);
     }
 
     gtk_widget_destroy(dialog);
-}*/
+}
+
+void show_text(char *stringFinale)
+{
+    GtkWidget *text = gtk_message_dialog_new(GTK_WINDOW(window),
+                        GTK_DIALOG_DESTROY_WITH_PARENT,
+                        GTK_MESSAGE_INFO, GTK_BUTTONS_NONE,
+                        "TEXT : %s", stringFinale);
+
+    gtk_window_set_title(GTK_WINDOW(text), "Text");
+    gtk_dialog_add_button(GTK_DIALOG(text), "Save file", GTK_RESPONSE_YES);
+
+    int result = gtk_dialog_run(GTK_DIALOG(text));
+    if (result == GTK_RESPONSE_YES)
+        save_file(stringFinale);
+
+    g_signal_connect_swapped (text, "response",
+        G_CALLBACK (gtk_widget_destroy), text);
+
+    gtk_widget_destroy(text);
+}
 
 // Lunch OCR
 
@@ -148,24 +187,18 @@ void lunch_ocr()
     // Apply Canny and find angle for rotate the image
     int angle = Hough_Transform(image_surface);
     image_surface = Rotate(image_surface, angle);
-    SDL_SaveBMP(image_surface, "image/seg_image-canny.bmp");
-    load_file("image/seg_image-binarized.bmp");
+    SDL_SaveBMP(image_surface, "image/seg_image-rotate.bmp");
+    load_file("image/seg_image-rotate.bmp");
 
     // Apply Segmentation
     Segmentation(image_surface,copy_surface,stringFinale);
-    printf("\nstring finaaale = %s\n",stringFinale);
+    SDL_SaveBMP(image_surface, "image/seg_image-segmentation.bmp");
+    load_file("image/seg_image-segmentation.bmp");
 
-    SDL_SaveBMP(image_surface, "image/seg_image-contour.bmp");
-    load_file("image/seg_image-contour.bmp");
+    printf("\nstring finaaale = %s\n", stringFinale);
 
-    GtkWidget *text = gtk_message_dialog_new(GTK_WINDOW(window),
-                        GTK_DIALOG_DESTROY_WITH_PARENT,
-                        GTK_MESSAGE_INFO,GTK_BUTTONS_CLOSE,
-                        "TEXT : %s",
-    stringFinale);
-    gtk_window_set_title(GTK_WINDOW(text), "Text");
-    gtk_dialog_run(GTK_DIALOG(text));
-    gtk_widget_destroy(text);
+    // Show text : Create dialog
+    show_text(stringFinale);
 
     // Final image
     SDL_SaveBMP(image_surface, "image/seg_image_ocr.bmp");
@@ -208,7 +241,7 @@ int gtk_init_window(int argc, char **argv)
     GtkWidget* rotate90_button = gtk_button_new_with_label("Rotate 90°");
 
     // Window parameter
-    gtk_window_set_position(GTK_WINDOW(window), GTK_WIN_POS_CENTER);
+    gtk_window_set_position(GTK_WINDOW(window), GTK_WIN_POS_CENTER_ALWAYS);
     gtk_window_set_title(GTK_WINDOW(window), "La compagnie des nez rouges <3");
     gtk_window_set_icon_from_file(GTK_WINDOW(window), "image/logo_OCR.png", NULL);
 
